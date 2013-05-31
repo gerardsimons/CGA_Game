d5852cfafae825fadf68bf3d37839e049249eaf2
Gerard Simons <sjrasimons@gmail.com>
2013-05-31 13:14:11 +0200

    New project setup with header files and main.cpp

---
+++ main.cpp
@@ -0,0 +1,141 @@
+#ifdef WIN32
+#include <Windows.h>
+#endif
+#include <GL/glut.h>
+#include <stdlib.h>
+#include <math.h>
+#include <assert.h>
+#include "traqueboule.h"
+#include "mesh.h"
+
+
+using namespace std;
+
+unsigned int W_fen = 800;  // largeur fenetre
+unsigned int H_fen = 800;  // hauteur fenetre
+int zNear = 1;
+int zFar = 10;
+
+float BackgroundColor[]={0,0,0};
+
+std::vector<Vec3Df> LightPos;
+std::vector<Vec3Df> LightColor;
+
+Vec3Df CamPos = Vec3Df(0.0f,0.0f,-4.0f);
+
+void keyboard(unsigned char key, int x, int y)
+{
+    printf("key %d pressed at %d,%d\n",key,x,y);
+}
+
+
+
+
+
+/************************************************************
+ * Appel des diff\E9rentes fonctions de dessin
+************************************************************/
+
+
+void dealWithUserInput(int x, int y)
+{
+
+}
+
+void draw( )
+{
+	//glutSolidSphere(1.0 ,10,10);
+}
+
+void idle()
+{
+	CamPos=getCameraPosition();
+	glutPostRedisplay();
+}
+
+void display(void);
+void reshape(int w, int h);
+void keyboard(unsigned char key, int x, int y);
+
+
+
+/************************************************************
+ * Programme principal
+ ************************************************************/
+int main(int argc, char** argv)
+{
+    glutInit (&argc, argv);
+
+
+    // couches du framebuffer utilisees par l'application
+    glutInitDisplayMode( GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH );
+
+    // position et taille de la fenetre
+    glutInitWindowPosition(200, 100);
+    glutInitWindowSize(W_fen,H_fen);
+    glutCreateWindow(argv[0]);	
+
+    // Initialisation du point de vue
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    glTranslatef(0,0,-4);
+    tbInitTransform();     // initialisation du point de vue
+    tbHelp();                      // affiche l'aide sur la traqueboule
+
+    glDisable( GL_LIGHTING );
+    glEnable(GL_COLOR_MATERIAL);
+    glEnable(GL_NORMALIZE);
+    
+    // cablage des callback
+    glutReshapeFunc(reshape);
+    glutKeyboardFunc(keyboard);
+    glutDisplayFunc(display);
+    glutMouseFunc(tbMouseFunc);    // traqueboule utilise la souris
+    glutMotionFunc(tbMotionFunc);  // traqueboule utilise la souris
+    glutIdleFunc(idle);
+
+
+    // Details sur le mode de trac\E9
+    glEnable( GL_DEPTH_TEST );            // effectuer le test de profondeur
+    glShadeModel(GL_SMOOTH);
+
+    // Effacer tout
+    glClearColor (BackgroundColor[0],BackgroundColor[1], BackgroundColor[2], 0.0);
+    glClear( GL_COLOR_BUFFER_BIT  | GL_DEPTH_BUFFER_BIT); // la couleur et le z
+   
+
+    // lancement de la boucle principale
+    glutMainLoop();
+
+    return 0;  // instruction jamais ex\E9cut\E9e
+}
+
+
+/************************************************************
+ * Fonctions de gestion opengl \E0 ne pas toucher
+ ************************************************************/
+// Actions d'affichage
+// Ne pas changer
+void display(void)
+{
+    glClear( GL_COLOR_BUFFER_BIT  | GL_DEPTH_BUFFER_BIT); // la couleur et le z
+
+	glLoadIdentity();  // repere camera
+
+    tbVisuTransform(); // origine et orientation de la scene
+
+    draw();    
+
+    glutSwapBuffers();
+}
+
+// pour changement de taille ou desiconification
+void reshape(int w, int h)
+{
+    glViewport(0, 0, (GLsizei) w, (GLsizei) h);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluPerspective (50, (float)w/h, zNear, zFar);
+    glMatrixMode(GL_MODELVIEW);
+}
+
